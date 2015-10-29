4c98c1e377e3df921c0c3936eac15907e2ce3b5c
SayHelloAndCleanMyShoes <dm.kazhura@gmail.com>
2015-10-29 12:27:24 +0300

    Initial Commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by Dmitriy Kazhura on 29/10/15.
+//  Copyright © 2015 Dmitriy Kazhura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by Dmitriy Kazhura on 29/10/15.
+//  Copyright © 2015 Dmitriy Kazhura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
