330a7748b133d7a3f83d986c80b298f2a47669ef
SayHelloAndCleanMyShoes <dm.kazhura@gmail.com>
2015-11-06 14:30:06 +0300

    Thumbnail generation created. Dateformatter initialization method created.

--- RSSReader/RSSNewsTableViewCell.m
+++ RSSReader/RSSNewsTableViewCell.m
@@ -22,12 +22,55 @@
 
 @implementation RSSNewsTableViewCell
 
+#pragma mark - Configuring news
+
 -(void) configureWithNews:(RSSNewsModel *)news {
     self.newsTitleLabel.text = news.newsTitle;
     self.newsAuthorLabel.text = news.newsAuthor;
     self.newsDateLabel.text = [NSString stringWithFormat:@"%@",[self.dateFormatter stringFromDate:news.newsDate]];
-    self.newsThumbnailImage.image = [UIImage imageWithData:[RSSNewsModel sharedModel].newsImageData];
+    self.newsThumbnailImage.image = [self generateThumgnailFromImage:
+                                     [UIImage imageWithData:[RSSNewsModel sharedModel].newsImageData]];
 }
+
+#pragma mark - Image -> Thumbnail
+
+-(UIImage *) generateThumgnailFromImage: (UIImage *) image {
+    UIImage *thumbnail;
+    CGSize destinationSize = CGSizeMake(160, 78);
+    UIGraphicsBeginImageContext(destinationSize);
+    [image drawInRect:CGRectMake(0, 0, destinationSize.width, destinationSize.height)];
+    thumbnail = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return thumbnail;
+}
+
+#pragma mark - dateFormatter init 
+
+
+-(NSDateFormatter *) dateFormatter {
+    static NSDateFormatter *dateFormatter = nil;
+    
+    if (dateFormatter == nil) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:60 * 60 * 3]];
+        [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+        [dateFormatter setTimeStyle:NSDateFormatterMediumStyle];
+    }
+    return dateFormatter;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @end
